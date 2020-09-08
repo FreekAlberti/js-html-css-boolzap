db644066c27473d1c3fcffb73044ca390634f0e8
FreekAlberti <federicoalberi96@gmail.com>
2020-09-08 21:30:16 +0200

    messaggi inviabili

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
 $(document).ready(function() {
-  
+  $(".inserimento-testo .fas").click(function() {
+    var inputValue = $("#addTest").val();
+    if (inputValue != "") {
+      var myClone = $(".linea-inviati .messaggi-inviati").clone();
+      myClone.text(inputValue);
+      $(".nuovi-messaggi").append(myClone);
+      $("#addTest").val("");
+    }
+  });
+
+  $("#addTest").keydown(function(event) {
+    if (event.which == 13) {
+      var inputValue = $("#addTest").val();
+      if (inputValue != "") {
+        var myClone = $(".linea-inviati .messaggi-inviati").clone();
+        myClone.text(inputValue);
+        $(".nuovi-messaggi").append(myClone);
+        $("#addTest").val("");
+      }
+    }
+  });
 });
